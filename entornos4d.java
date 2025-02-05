811ee17f9a1b5da880caff948a717af2e20edc11
SergioBadia <sergarba16@gmail.com>
2025-02-05 12:48:30 +0100

    Valor añadido a las variables

--- entornos4d.java
+++ entornos4d.java
@@ -1,7 +1,7 @@
 public class entornos4d {
 	public static void main(String[] args) {
-        int a;
-        int b;
+        int a = 1;
+        int b = 3;
         System.out.println("Hola Mundo");
 
     }
